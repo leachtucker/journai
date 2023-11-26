4d5cc6d11ab0cafb454b9425c31ca9520d14439f
Tucker Leach <leachtucker@gmail.com>
2023-11-26 14:18:08 -0800

    feat: :construction: Setup dashboard layout and basic journal page

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import { UserButton } from '@clerk/nextjs';
+
+type LayoutProps = { children: React.ReactNode };
+const Layout = ({ children }: LayoutProps) => {
+	return (
+		<div className="w-screen h-screen relative">
+			<aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
+				JournAI
+			</aside>
+
+			<div className="ml-[200px]">
+				<header className="h-[60px] border-b border-black/10">
+					<div className="h-full w-full px-6 flex items-center justify-end">
+						<UserButton />
+					</div>
+				</header>
+
+				<div>{children}</div>
+			</div>
+		</div>
+	);
+};
+
+export default Layout;
