86099904455905defbdd97a7bcf2e62668ba56c0
Tucker Leach <leachtucker@gmail.com>
2023-11-28 20:51:34 -0800

    feat: :construction: Scaffold entry anaylsis display

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -15,10 +15,39 @@ const Page = async ({ params }: PageProps) => {
 	}
 
 	return (
-		<div className="h-full w-full">
-			<Editor entry={entry} />
+		<div className="h-full w-full grid grid-cols-3">
+			<div className="col-span-2 border-r border-black/10">
+				<Editor entry={entry} />
+			</div>
+
+			<div className="col-span-1">
+				<div className="bg-blue-300 px-6 py-10">
+					<h2 className="text-xl">Analysis</h2>
+				</div>
+
+				<div>
+					<ul>
+						{analysisInfo.map((item) => (
+							<li
+								key={item.name}
+								className="flex items-center justify-between px-2 py-4 border-b border-black/10"
+							>
+								<span className="text-lg">{item.name}</span>
+								<span>{item.value}</span>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
 
 export default Page;
+
+const analysisInfo = [
+	{ name: 'Subject', value: '' },
+	{ name: 'Summary', value: '' },
+	{ name: 'Mood', value: '' },
+	{ name: 'Negative', value: '' },
+];
