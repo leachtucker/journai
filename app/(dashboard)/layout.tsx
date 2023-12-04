5272587e37738e51f650f58c9e9d429ea8958eb4
Tucker Leach <leachtucker@gmail.com>
2023-12-03 18:04:04 -0800

    feat: :sparkles: Implement basic nav and logo

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,33 @@
 import { UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
 
 type LayoutProps = { children: React.ReactNode };
 const Layout = ({ children }: LayoutProps) => {
 	return (
 		<div className="w-screen h-screen relative">
 			<aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
-				JournAI
+				<nav>
+					<Link href="/">
+						<div className="py-4 flex justify-center bg-black">
+							<h2 className="text-2xl font-semibold text-white">
+								Journ<span className="text-indigo-500 font-bold">AI</span>
+							</h2>
+						</div>
+					</Link>
+
+					<ul>
+						{links.map((link) => (
+							<li
+								key={link.href}
+								className="py-3 flex justify-center border-b border-black/10"
+							>
+								<Link href={link.href}>
+									<span className="text-md font-semibold ">{link.label}</span>
+								</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
 			</aside>
 
 			<div className="ml-[200px] h-full">
@@ -22,3 +44,5 @@ const Layout = ({ children }: LayoutProps) => {
 };
 
 export default Layout;
+
+const links = [{ href: '/journal', label: 'Journal' }];
