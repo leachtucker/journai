672ce7eaec5ce003a4afae548108fe0fe4ca6f97
Tucker Leach <leachtucker@gmail.com>
2023-12-03 17:41:20 -0800

    perf: :zap: Do not peform debounce side-effect if val is same

--- components/hooks/useDebounce.ts
+++ components/hooks/useDebounce.ts
@@ -4,15 +4,21 @@ type UseDebounceProps = { value: unknown; wait: number; onWaitEnd: () => void };
 
 const useDebounce = ({ value, wait, onWaitEnd }: UseDebounceProps) => {
 	const timerRef = React.useRef<NodeJS.Timeout>();
+	const prevValueAtWaitEnd = React.useRef<unknown>(value);
 	React.useEffect(() => {
+		if (value == prevValueAtWaitEnd.current) return;
 		if (timerRef.current) {
 			clearTimeout(timerRef.current);
 		}
 
-		const timer = setTimeout(onWaitEnd, wait);
+		const timer = setTimeout(() => {
+			onWaitEnd();
+			prevValueAtWaitEnd.current = value;
+		}, wait);
+
 		timerRef.current = timer;
 
-		() => {
+		return () => {
 			clearTimeout(timerRef.current);
 		};
 	}, [value, wait, onWaitEnd]);
