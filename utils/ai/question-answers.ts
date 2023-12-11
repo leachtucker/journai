9a3a4cf023c92b6c295ac4d5e684f1044a4edd33
Tucker Leach <leachtucker@gmail.com>
2023-12-10 18:09:47 -0800

    feat: :alembic: Engineering prompt for more personable answers

--- utils/ai/question-answers.ts
+++ utils/ai/question-answers.ts
@@ -4,10 +4,28 @@ import { OpenAI } from 'langchain/llms/openai';
 import { loadQARefineChain } from 'langchain/chains';
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PromptTemplate } from 'langchain/prompts';
 
 const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' });
-const chain = loadQARefineChain(model);
 const embeddings = new OpenAIEmbeddings();
+
+const questionPromptTemplateString = `Context information is below. 
+-------------------------
+{context}
+-------------------------
+The context information is my personal journal entries that I have written about my life. The information can come from various days.
+I am going to ask you a question. Answer the provided question by considering all of my journal entries.
+Do not answer questions that are not related to myself or my journal entries.
+Never mention "context" in your answer! 
+
+Answer the question: {question}`;
+
+const questionPrompt = new PromptTemplate({
+	inputVariables: ['context', 'question', 'currentDate'],
+	template: questionPromptTemplateString,
+});
+
+const chain = loadQARefineChain(model, { questionPrompt });
 
 export const askQuestionAgainstEntries = async (
 	question: string,
@@ -29,11 +47,12 @@ export const askQuestionAgainstEntries = async (
 		embeddings
 	);
 
-	const relatedDocuments = await vectorStore.similaritySearch(question);
+	const relatedDocuments = await vectorStore.similaritySearch(question, 5);
 
 	const response = await chain.call({
 		input_documents: relatedDocuments,
 		question,
+		currentDate: new Date().toLocaleDateString(),
 	});
 
 	return response.output_text;
