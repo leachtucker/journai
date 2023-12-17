b49e25cc96c76a77d5dce048001c7a94b4c8b568
Tucker Leach <leachtucker@gmail.com>
2023-12-16 16:37:06 -0800

    test: :white_check_mark: Test home page logged-in vs logged-out

--- tests/app/landing-page.test.tsx
+++ tests/app/landing-page.test.tsx
@@ -3,27 +3,42 @@ import { vi } from 'vitest';
 
 import LandingPage from '../../app/page';
 
+const mocks = vi.hoisted(() => {
+	return {
+		auth: vi.fn(),
+		ClerkProvider: vi.fn(),
+		useUser: vi.fn(),
+	};
+});
+
+vi.mock('@clerk/nextjs', () => {
+	return mocks;
+});
+
 const CLERK_USER_FIXTURE = {
 	id: 'user.id.123',
 	fullName: 'John Doe',
 };
 
-vi.mock('@clerk/nextjs', () => {
-	return {
-		auth: async () => ({
-			userId: CLERK_USER_FIXTURE.id,
-		}),
-		ClerkProvider: ({ children }: React.PropsWithChildren) => <>{children}</>,
-		useUser: () => ({
-			isSignedIn: true,
-			user: CLERK_USER_FIXTURE,
-		}),
-	};
-});
+const setupClerkMocks = (isSignedIn: boolean) => {
+	mocks.auth.mockImplementation(async () => ({
+		userId: isSignedIn ? CLERK_USER_FIXTURE.id : null,
+	}));
+
+	mocks.useUser.mockImplementation(() => ({
+		isSignedIn,
+		user: isSignedIn ? CLERK_USER_FIXTURE : null,
+	}));
+
+	mocks.ClerkProvider.mockImplementation(
+		({ children }: React.PropsWithChildren) => <>{children}</>
+	);
+};
 
 describe('Landing Page', async () => {
-	describe('signed in', async () => {
+	describe('logged-in', async () => {
 		beforeEach(async () => {
+			setupClerkMocks(true);
 			render(await LandingPage());
 		});
 
@@ -36,6 +51,20 @@ describe('Landing Page', async () => {
 			const href = ctaEl.getAttribute('href');
 
 			expect(href).toBe('/journal');
+		});
+	});
+
+	describe('logged-out', async () => {
+		beforeEach(async () => {
+			setupClerkMocks(false);
+			render(await LandingPage());
+		});
+
+		it('CTA link points to the new user page', async () => {
+			const ctaEl = screen.getByTestId('ctaLink');
+			const href = ctaEl.getAttribute('href');
+
+			expect(href).toBe('/new-user');
 		});
 	});
 });
